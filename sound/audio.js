e28e1b8fbad632aaecece0c2295c98dcad079f0c
Jaineel Upadhyay <32003330+ujaineel@users.noreply.github.com>
2020-07-12 17:02:25 -0400

    Add files via upload

--- sound/audio.js
+++ sound/audio.js
@@ -1,1 +1,54 @@
+function setCookie(c_name,value,exdays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=value + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+}
 
+function getCookie(c_name)
+{
+    var i,x,y,ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++)
+    {
+      x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+      y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+      x=x.replace(/^\s+|\s+$/g,"");
+      if (x==c_name)
+        {
+        return y;
+        }
+      }
+}
+
+var song = document.getElementById("bgm");
+var played = false;
+var tillPlayed = getCookie('timePlayed');
+function update()
+{
+    if (!window){
+      song.pause();
+      song.currentTime = 0;
+    }
+    if(!played){
+        if(tillPlayed){
+        song.currentTime = tillPlayed;
+        song.play();
+        played = true;
+        }
+        else {
+                song.play();
+                played = true;
+        }
+    }
+
+    else {
+    setCookie('timePlayed', song.currentTime);
+    }
+}
+setInterval(update,0);
+
+function stopBGM(){
+	song.pause();
+	song.currentTime = 0;
+}
